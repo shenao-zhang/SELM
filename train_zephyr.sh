f2c5c3b8593f8349c9e91e5e300bc2623a7400cd
shenao-zhang <82402331+shenao-zhang@users.noreply.github.com>
2024-05-31 12:18:19 -0500

    fix name error

--- train_zephyr.sh
+++ train_zephyr.sh
@@ -22,7 +22,7 @@ for i in $(seq 1 $iter_num); do
     fi
     output_dir="data/$hub_model_id"
     if [ "$i" -eq 1 ]; then
-        ACCELERATE_LOG_LEVEL=info /home/aiscuser/.local/bin/accelerate launch --config_file recipes/accelerate_configs/deepspeed_zero3.yaml scripts/run_dpo.py recipes/zephyr-7b-beta-selm/dpo_config_full.yaml dataset_splits=$dataset_splits hub_model_id=$hub_model_id output_dir=$output_dir || exit 1
+        ACCELERATE_LOG_LEVEL=info /home/aiscuser/.local/bin/accelerate launch --config_file recipes/accelerate_configs/deepspeed_zero3.yaml scripts/run_dpo.py recipes/zephyr-selm/dpo_config_full.yaml dataset_splits=$dataset_splits hub_model_id=$hub_model_id output_dir=$output_dir || exit 1
     else
         python scripts/online_feedback.py recipes/zephyr-selm/selm_config_full.yaml learning_rate=$learning_rate model_name_or_path=$model_name_or_path dataset_mixer=$dataset_mixer dataset_splits=$dataset_splits || exit 1
         ACCELERATE_LOG_LEVEL=info /home/aiscuser/.local/bin/accelerate launch --config_file recipes/accelerate_configs/deepspeed_zero3.yaml scripts/run_selm.py recipes/zephyr-selm/selm_config_full.yaml learning_rate=$learning_rate alpha=$alpha model_name_or_path=$model_name_or_path dataset_mixer=$dataset_mixer hub_model_id=$hub_model_id output_dir=$output_dir || exit 1
