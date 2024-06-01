f7ad7e66a173238807b0b075ac1b05884ac6b8c7
Shenao Zhang (CSI INTERFUSION INC) <v-zhangshen@microsoft.com>
2024-06-01 11:42:51 -0500

    fix name error

--- train_llama.sh
+++ train_llama.sh
@@ -19,5 +19,5 @@ for i in $(seq 1 $iter_num); do
         learning_rate=1e-7
     fi
     python scripts/online_feedback.py recipes/llama3-selm/selm_config_full.yaml learning_rate=$learning_rate model_name_or_path=$model_name_or_path dataset_mixer=$dataset_mixer dataset_splits=$dataset_splits || exit 1
-    ACCELERATE_LOG_LEVEL=info /home/aiscuser/.local/bin/accelerate launch --config_file recipes/accelerate_configs/deepspeed_zero3.yaml scripts/run_selm.py recipes/llama3-selm/selm_config_full.yaml learning_rate=$learning_rate alpha=$alpha model_name_or_path=$model_name_or_path dataset_mixer=$dataset_mixer hub_model_id=$hub_model_id output_dir=$output_dir || exit 1
+    ACCELERATE_LOG_LEVEL=info /home/aiscuser/.local/bin/accelerate launch --config_file recipes/accelerate_configs/deepspeed_zero3.yaml scripts/run_selm.py recipes/llama3-selm/selm_config_full.yaml learning_rate=$learning_rate model_name_or_path=$model_name_or_path dataset_mixer=$dataset_mixer hub_model_id=$hub_model_id output_dir=$output_dir || exit 1
 done
